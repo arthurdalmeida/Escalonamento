af957a9a3db8ca5876d7e171e72d6252c8a1351a
arthurdalmeida <aao@cesar.school>
2026-09-06 13:30:16 -0300

    validando arquivo e tempo

--- escalonamento.c
+++ escalonamento.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MaximoDeTarefas 100;
+#define MaximoDeTarefas 100
 
 typedef struct {
     char nome[50];
@@ -13,10 +13,34 @@ typedef struct {
     int prazo;
 } tarefa;
 
+FILE* abrirArquivo(const char *caminho){
+    FILE *arquivo = fopen(caminho, "r");
+
+    if (arquivo == NULL){
+        fprintf(stderr, "Não foi possível abrir o arquivo: %s\n", caminho);
+    }
+
+    return arquivo;
+}
+
+int lerArquivo(FILE *arquivo, int *tempoTotal){
+    if (fscanf(arquivo, "%d", tempoTotal) != 1){
+        fprintf(stderr, "Não foi possível ler o tempo total.\n");
+        return 0;
+    }
+
+    if (*tempoTotal <= 0){
+        fprintf(stderr, "O tempo total deve ser maior que zero.\n");
+        return 0;
+    }
+
+    return 1;
+}
+
 
 int main(int argc, char *argv[]){
     if (argc != 3){
-        fprintf(stderr, "Uso: %s <rate|edf> <arquivo>", argv[0]);
+        fprintf(stderr, "Uso: %s <rate|edf> <arquivo>\n", argv[0]);
         return 1;
     }
 
@@ -24,6 +48,21 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Algoritmo inválido.\n");
         return 1;
     }
+
+    FILE *arquivo = abrirArquivo(argv[2]);
+
+    if (arquivo == NULL){
+        return 1;
+    }
+
+    int tempoTotal;
+
+    if (lerArquivo(arquivo, &tempoTotal) == 0){
+        fclose(arquivo);
+        return 1;
+    }
+
+    fclose(arquivo);
 
     return 0;
 }
